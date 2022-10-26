82772d17680722ac95692eeef279bd93cd30d652
Julesemery <j.emery@xefi.fr>
2022-10-26 16:53:01 +0200

    score

---
+++ lib/data/models/quizz.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'quizz.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Quizz _$$_QuizzFromJson(Map<String, dynamic> json) => _$_Quizz(
+      json['listquestion'] as List<Question>,
+      json['score'] as int,
+    );
+
+Map<String, dynamic> _$$_QuizzToJson(_$_Quizz instance) => <String, dynamic>{
+      'listquestion': instance.listquestion,
+      'score': instance.score,
+    };
